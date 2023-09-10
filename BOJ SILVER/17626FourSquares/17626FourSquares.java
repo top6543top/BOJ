fdf7f356249983abd9302e5166c58b0651aa096a
top6543top <111718237+top6543top@users.noreply.github.com>
2023-09-10 16:41:32 +0900

    Update 17626FourSquares.java

--- BOJ SILVER/17626FourSquares/17626FourSquares.java
+++ BOJ SILVER/17626FourSquares/17626FourSquares.java
@@ -10,11 +10,11 @@ public class Main
         int min=0,b=0,flag=0;
         for(int i=1;i<=a;i++){
             if(Math.sqrt(i)==Math.floor(Math.sqrt(i))){
-                num[i]=1;
-                s.add(i);
-            }else{
-                for(int j=s.size()-1;j>=0;j--){
-                    flag=num[s.get(j)]+num[i-s.get(j)];
+                num[i]=1; //제곱수인 것은 1개로 할당
+                s.add(i); //제곱수만 있는 것은 arraylist에 넣는다.
+            }else{ // 제곱수가 아닌것들
+                for(int j=s.size()-1;j>=0;j--){ // 다이나믹 프로그래밍 알고리즘 이용
+                    flag=num[s.get(j)]+num[i-s.get(j)]; // 배열안에 있는 제곱수를 더해주고 그 전에 있는 것 더해준다
                     if(j==s.size()-1){min=flag;}
                     else {
                         if(min>flag){min=flag;}
